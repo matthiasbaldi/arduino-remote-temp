06bd4b32cb2e23fd9fbffbce3a09e50eb462514a
Matthias Baldi <matthias.baldi@secanis.ch>
2019-06-03 23:29:38 +0200

    switch sender library

--- temperatur/temperatur.ino
+++ temperatur/temperatur.ino
@@ -1,26 +1,19 @@
-/********************************************************************/
-// First we include the libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <TM1637Display.h>
-#include <RCSwitch.h>
-/********************************************************************/
+#include <RH_ASK.h>
+#include <SPI.h>
+
 // Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 2
 #define CLK 6
 #define DIO 7
 TM1637Display display(CLK, DIO);
-
-/********************************************************************/
-// Setup a oneWire instance to communicate with any OneWire devices
+RH_ASK rf_driver;
 OneWire oneWire(ONE_WIRE_BUS);
-/********************************************************************/
-// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-/********************************************************************/
-RCSwitch sender = RCSwitch();
+
 void setup(void) {
-  /********************************************************************/
   // activate jumper for device identification
   pinMode(A0, INPUT_PULLUP);
   pinMode(A1, INPUT_PULLUP);
@@ -31,11 +24,11 @@ void setup(void) {
   pinMode(A6, INPUT_PULLUP);
   // start serial port
   Serial.begin(9600);
-  Serial.println("Dallas Temperature IC Control Library Demo");
   sensors.begin();
-  // start transmitter
-  sender.enableTransmit(10);
+  rf_driver.init();
 }
+
+String str_out;
 
 void loop(void) {
   display.setBrightness(0x0f);
@@ -51,40 +44,22 @@ void loop(void) {
 
   // call sensors.requestTemperatures() to issue a global temperature
   // request to all devices on the bus
-  /********************************************************************/
-  //"DeviceID: " + 
   String deviceID = String(DEVICEID);
   Serial.println(String("DeviceID: " + deviceID));
   Serial.print(" Requesting temperatures...");
   sensors.requestTemperatures(); // Send the command to get temperature readings
   Serial.println("DONE");
-  /********************************************************************/
   Serial.print("Temperature is: ");
   int temp = sensors.getTempCByIndex(0) * 100;
   Serial.println(temp);
-  String tempString = String(temp);
-  String transmitValue = String("gmonit;" + deviceID + ";" + tempString);
+  str_out = "gmonit;" + String(deviceID) + ";" + String(temp);
+  char *transmitValue = str_out.c_str();
   Serial.println(transmitValue);
-  // showNumberDecEx(int num, uint8_t dots = 0, bool leading_zero = false, uint8_t length = 4, uint8_t pos = 0);
   display.showNumberDecEx(temp, 0b11100000, false, 4, 0);
-  // send number over network
+  Serial.println(strlen(transmitValue));
 
-  char* pEnd;
-/*  byte *p = transmitValue.c_str();
-  String binString = "";
-  while (*p != 0)  {
-    binString = String(binString + String(*p, BIN)); p++;
-  }*/
-  Serial.println(transmitValue);
-  byte val = strtol(transmitValue.c_str(), &pEnd, 2);
-  sender.send(val);
-  
-  /*int i = 0;
-  const char* str = transmitValue.c_str();
-  for (char *p = str; *p; p++) {
-      sender.send((i<<8) + *p, 16);
-      i++;
-  }*/
-  // sender.send(transmitValue, 24);
-  delay(5000);
+  // send data over 433Mhz network
+  rf_driver.send((uint8_t *)transmitValue, strlen(transmitValue));
+  rf_driver.waitPacketSent();
+  delay(20000);
 }
